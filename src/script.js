848d9eb2b19d72ca5bbe915027f7de1e042fb2f5
MattCod3s <matthew.kostka08@gmail.com>
2021-04-17 01:46:14 +0100

    lesson 10 on textures basic setup

--- src/script.js
+++ src/script.js
@@ -1,20 +1,7 @@
 import './style.css'
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
-import gsap from 'gsap'
-import * as dat from 'dat.gui'
 
-//Debug
-const gui = new dat.GUI()
-
-
-
-const parameters = {
-    color: 0xff0000,
-    spin: () => {
-        gsap.to(mesh.rotation, { duration: 1, y: mesh.rotation.y + 10})
-    }
-}
 /**
  * Base
  */
@@ -28,17 +15,9 @@ const scene = new THREE.Scene()
  * Object
  */
 const geometry = new THREE.BoxGeometry(1, 1, 1)
-const material = new THREE.MeshBasicMaterial({ color: parameters.color })
+const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
 const mesh = new THREE.Mesh(geometry, material)
 scene.add(mesh)
-
-gui.add(mesh.position, 'y').min(-3).max(3).step(0.01)
-gui.add(mesh, 'visible')
-gui.add(material, 'wireframe')
-gui.addColor(parameters, 'color').onChange(() => {
-    material.color.set(parameters.color)
-})
-gui.add(parameters, 'spin')
 
 /**
  * Sizes
@@ -68,7 +47,9 @@ window.addEventListener('resize', () =>
  */
 // Base camera
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
-camera.position.z = 3
+camera.position.x = 1
+camera.position.y = 1
+camera.position.z = 1
 scene.add(camera)
 
 // Controls
