70245f0d3d284ed63a894a48cb55ad4eac15b653
MattCod3s <matthew.kostka08@gmail.com>
2021-04-22 17:59:25 +0100

    Lesson 14 on Lighting finished

--- src/script.js
+++ src/script.js
@@ -2,6 +2,7 @@ import './style.css'
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import * as dat from 'dat.gui'
+import {RectAreaLightHelper} from 'three/examples/jsm/helpers/RectAreaLightHelper'
 
 /**
  * Base
@@ -18,14 +19,34 @@ const scene = new THREE.Scene()
 /**
  * Lights
  */
-const ambientLight = new THREE.AmbientLight(0xffffff, 0.5)
-scene.add(ambientLight)
 
-const pointLight = new THREE.PointLight(0xffffff, 0.5)
-pointLight.position.x = 2
-pointLight.position.y = 3
-pointLight.position.z = 4
-scene.add(pointLight)
+const ambientLight = new THREE.AmbientLight(0xffffff, 0.5)
+const directionalLight = new THREE.DirectionalLight(0x00fffc, 0.3)
+const hemisphereLight = new THREE.HemisphereLight(0xff0000, 0x0000ff, 0.3)
+const pointLight = new THREE.PointLight(0xff9000, 0.5)
+const rectAreaLight = new THREE.RectAreaLight(0x4e00ff, 2, 3, 3)
+const spotLight = new THREE.SpotLight(0x78ff00, 0.5, 10, Math.PI * 0.1, 0.25, 1)
+spotLight.position.set(0, 2, 3)
+//rectAreaLight.position.set(-1.5, 0, 1.5)
+//rectAreaLight.lookAt(new THREE.Vector3())
+//directionalLight.position.set(1, 0.25, 0)
+
+scene.add(ambientLight, directionalLight, hemisphereLight, pointLight, rectAreaLight, spotLight)
+
+
+//Heplers
+// const directionalLightHelper = new THREE.DirectionalLightHelper(directionalLight, 0.2)
+// const hemisphereLightHelper = new THREE.HemisphereLightHelper(hemisphereLight, 0.2)
+// const spotLightHelper = new THREE.SpotLightHelper(spotLight)
+// const rectAreaLightHelper = new RectAreaLightHelper(rectAreaLight)
+
+
+// window.requestAnimationFrame(()=> {
+//     spotLightHelper.update()
+// })
+
+// scene.add(directionalLightHelper, hemisphereLightHelper, spotLightHelper, rectAreaLightHelper)
+
 
 /**
  * Objects
