505323ae4fad29857773617e0f15f111ccbf9c88
MattCod3s <matthew.kostka08@gmail.com>
2021-04-27 23:12:28 +0100

    test

--- src/script.js
+++ src/script.js
@@ -40,6 +40,10 @@ material.roughness = 0.7
 gui.add(material, 'metalness').min(0).max(1).step(0.001)
 gui.add(material, 'roughness').min(0).max(1).step(0.001)
 
+/*need to do sometihng for hithub so this is usesells*/
+console.log('hi')
+
+
 /**
  * Objects
  */
