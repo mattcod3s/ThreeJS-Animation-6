3511fe7746e478f7c8c7b7d65d01664c404cdf2f
MattCod3s <matthew.kostka08@gmail.com>
2021-04-30 21:42:37 +0100

    Lesson 18 - Galaxy Generator basic setup

--- src/script.js
+++ src/script.js
@@ -16,67 +16,13 @@ const canvas = document.querySelector('canvas.webgl')
 const scene = new THREE.Scene()
 
 /**
- * Textures
- */
-const textureLoader = new THREE.TextureLoader()
-
-const particleTexture = textureLoader.load('/textures/particles/3.png')
-
-/**
- * Particles
- */
-
-const particlesGeometry = new THREE.BufferGeometry()
-const count = 5000
-
-const positions = new Float32Array(count * 3)
-const colors = new Float32Array(count * 3)
-
-for(let i = 0; i < count * 3; i++) {
-    positions[i] = (Math.random() - 0.5) * 10
-    colors[i] = Math.random()
-}
-
-particlesGeometry.setAttribute('position', new THREE.BufferAttribute(positions, 3))
-particlesGeometry.setAttribute('color', new THREE.BufferAttribute(colors, 3))
-
-const particlesMaterial = new THREE.PointsMaterial({
-    size: 0.1,
-    sizeAttenuation: true,
-    //color: 0xff00ff,
-    alphaMap: particleTexture,
-    transparent: true,
-    depthWrite: false,
-    vertexColors: true
-    //blending: THREE.AdditiveBlending
-    //depthTest: false
-    //alphaTest: 0.001
-})
-
-const particles = new THREE.Points(particlesGeometry, particlesMaterial)
-
-scene.add(particles)
-
-// })
-// const particlesGeometry = new THREE.SphereGeometry(1, 32, 32)
-// const particlesMaterial = new THREE.PointsMaterial({
-//     size: 0.02,
-//     sizeAttenuation: true
-// })
-
-// const particles = new THREE.Points(particlesGeometry, particlesMaterial)
-
-// scene.add(particles)
-
-
-/**
  * Test cube
  */
-// const cube = new THREE.Mesh(
-//     new THREE.BoxGeometry(1, 1, 1),
-//     new THREE.MeshBasicMaterial()
-// )
-// scene.add(cube)
+const cube = new THREE.Mesh(
+    new THREE.BoxGeometry(1, 1, 1),
+    new THREE.MeshBasicMaterial()
+)
+scene.add(cube)
 
 /**
  * Sizes
@@ -106,6 +52,8 @@ window.addEventListener('resize', () =>
  */
 // Base camera
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
+camera.position.x = 3
+camera.position.y = 3
 camera.position.z = 3
 scene.add(camera)
 
@@ -130,17 +78,6 @@ const clock = new THREE.Clock()
 const tick = () =>
 {
     const elapsedTime = clock.getElapsedTime()
-
-    // Update Particles
-    //particles.rotation.y = elapsedTime * 0.2
-    for(let i = 0; i < count; i++) {
-        const i3 = i * 3
-
-        const x = particlesGeometry.attributes.position.array[i3 + 0]
-        particlesGeometry.attributes.position.array[i3 + 1] = Math.sin(elapsedTime + x)
-    }
-
-    particlesGeometry.attributes.position.needsUpdate = true
 
     // Update controls
     controls.update()
