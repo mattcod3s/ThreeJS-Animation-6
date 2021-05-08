06e28b8de164d99ee4680ec69132d1e75ef4308c
MattCod3s <matthew.kostka08@gmail.com>
2021-05-08 13:24:14 +0100

    Lesson 20 - easier way to apply contactMaterial, useful if physics world is simple and everything is made of the same material

--- src/script.js
+++ src/script.js
@@ -42,18 +42,18 @@ const world = new CANNON.World()
 world.gravity.set(0, -9.82, 0)
 
 // Materials
-const concreteMaterial = new CANNON.Material('concrete')
-const plasticMaterial = new CANNON.Material('plastic')
+const defaultMaterial = new CANNON.Material('default')
 
-const concretePlasticContactMaterial = new CANNON.ContactMaterial(
-    concreteMaterial,
-    plasticMaterial,
+const defaultContactMaterial = new CANNON.ContactMaterial(
+    defaultMaterial,
+    defaultMaterial,
     {
         friction: 0.1,
         restitution: 0.7
     }
 )
-world.addContactMaterial(concretePlasticContactMaterial)
+world.addContactMaterial(defaultContactMaterial)
+world.defaultContactMaterial = defaultContactMaterial
 
 //Sphere
 const sphereShape = new CANNON.Sphere(0.5)
@@ -61,7 +61,6 @@ const sphereBody = new CANNON.Body({
     mass: 1, 
     position: new CANNON.Vec3(0, 3, 0),
     shape: sphereShape,
-    material: plasticMaterial
 
 })
 
@@ -72,7 +71,6 @@ const floorShape = new CANNON.Plane()
 const floorBody = new CANNON.Body({
     mass: 0,
     shape: floorShape,
-    material: concreteMaterial
 })
 floorBody.quaternion.setFromAxisAngle(new CANNON.Vec3(-1, 0, 0), Math.PI * 0.5)
 world.addBody(floorBody)
