1a3937f283ebdb58a6a1914f9cdf393f3a2256d3
MattCod3s <matthew.kostka08@gmail.com>
2021-05-13 21:23:04 +0100

    Lesson 23 on Realsitic Renders finished

--- src/script.js
+++ src/script.js
@@ -3,6 +3,7 @@ import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import * as dat from 'dat.gui'
 import {GLTFLoader} from 'three/examples/jsm/loaders/GLTFLoader.js'
+import { NoToneMapping } from 'three'
 
 /**
  * Base
@@ -27,8 +28,10 @@ const updateAllMaterials = () => {
         if(child instanceof THREE.Mesh && child.material instanceof THREE.MeshStandardMaterial) {
             child.material.envMap = envMap
             child.material.envMapIntensity = debugObject.envMapIntensity
-            
-        }
+            child.material.needsUpdate = true
+            child.castShadow = true
+            child.receiveShadow = true
+        }  
     })
 
 }
@@ -45,17 +48,21 @@ const envMap = cubeTextureLoader.load([
     '/textures/environmentMaps/0/pz.jpg',
     '/textures/environmentMaps/0/nz.jpg',
 ])
-
+envMap.encoding = THREE.sRGBEncoding
 scene.background = envMap
+scene.environment = envMap
+
 debugObject.envMapIntensity = 5
 gui.add(debugObject, 'envMapIntensity').min(0).max(12).step(0.001).onChange(updateAllMaterials)
 
 // Models
 gltfLoader.load(
-    '/models/FlightHelmet/glTF/FlightHelmet.gltf',
+    //'/models/hamburger.glb',
+    '/models/untitledburger.glb',
     (gltf) => {
-        gltf.scene.scale.set(10, 10, 10)
-        gltf.scene.position.set(0, -4, 0)
+        //gltf.scene.scale.set(0.3, 0.3, 0.3)
+        gltf.scene.scale.set(1, 1, 1)
+        gltf.scene.position.set(0, -1, 0)
         gltf.scene.rotation.y = Math.PI * 0.5
         scene.add(gltf.scene)
 
@@ -72,6 +79,10 @@ gltfLoader.load(
  */
 const directionalLight = new THREE.DirectionalLight('#ffffff', 3)
 directionalLight.position.set(0.25, 3, -2.25)
+directionalLight.castShadow = true
+directionalLight.shadow.camera.far = 15
+directionalLight.shadow.mapSize.set(1024, 1024)
+directionalLight.shadow.normalBias = 0.05
 scene.add(directionalLight)
 
 gui.add(directionalLight, 'intensity').min(0).max(10).step(0.001).name('lightIntensity')
@@ -118,11 +129,31 @@ controls.enableDamping = true
  * Renderer
  */
 const renderer = new THREE.WebGLRenderer({
-    canvas: canvas
+    canvas: canvas,
+    antialias: true
 })
 renderer.setSize(sizes.width, sizes.height)
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 renderer.physicallyCorrectLights = true
+renderer.outputEncoding = THREE.sRGBEncoding
+renderer.toneMapping = THREE.ACESFilmicToneMapping
+renderer.toneMappingExposure = 2
+renderer.shadowMap.enabled = true
+renderer.shadowMap.type  = THREE.PCFSoftShadowMap
+
+gui.add(renderer, 'toneMappingExposure').min(0).max(10).step(1).name('toneMapIntensity')
+
+gui.add(renderer, 'toneMapping', {
+    No: THREE.NoToneMapping,
+    Linear: THREE.LinearToneMapping,
+    Reinhard: THREE.ReinhardToneMapping,
+    Cineon: THREE.CineonToneMapping,
+    ACESFilmic: THREE.ACESFilmicToneMapping
+
+}).onFinishChange(() => {
+    renderer.toneMapping = Number(renderer.toneMapping)
+    updateAllMaterials()
+})
 
 /**
  * Animate
