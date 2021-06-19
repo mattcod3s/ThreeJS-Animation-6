5543df959223683a45a1aaeaa4869a3c5a28359c
MattCod3s <matthew.kostka08@gmail.com>
2021-06-19 21:14:38 +0100

    Lesson 28 - Modified Materials Basic Setup

--- src/script.js
+++ src/script.js
@@ -1,9 +1,8 @@
 import './style.css'
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js'
 import * as dat from 'dat.gui'
-import galaxyFragmentShader from './shaders/galaxy/fragment.glsl'
-import galaxyVertexShader from './shaders/galaxy/vertex.glsl'
 
 /**
  * Base
@@ -18,113 +17,89 @@ const canvas = document.querySelector('canvas.webgl')
 const scene = new THREE.Scene()
 
 /**
- * Galaxy
+ * Loaders
  */
-const parameters = {}
-parameters.count = 200000
-parameters.size = 0.005
-parameters.radius = 4
-parameters.branches = 5
-parameters.spin = 1
-parameters.randomness = 2.0
-parameters.randomnessPower = 6
-parameters.insideColor = '#0de1e1'
-parameters.outsideColor = '#6c02e1'
+const textureLoader = new THREE.TextureLoader()
+const gltfLoader = new GLTFLoader()
+const cubeTextureLoader = new THREE.CubeTextureLoader()
 
-let geometry = null
-let material = null
-let points = null
-
-const generateGalaxy = () =>
+/**
+ * Update all materials
+ */
+const updateAllMaterials = () =>
 {
-    if(points !== null)
+    scene.traverse((child) =>
     {
-        geometry.dispose()
-        material.dispose()
-        scene.remove(points)
-    }
-
-    /**
-     * Geometry
-     */
-    geometry = new THREE.BufferGeometry()
-
-    const positions = new Float32Array(parameters.count * 3)
-    const randomness = new Float32Array(parameters.count * 3)
-    const colors = new Float32Array(parameters.count * 3)
-    const scales = new Float32Array(parameters.count * 1)
-
-    const insideColor = new THREE.Color(parameters.insideColor)
-    const outsideColor = new THREE.Color(parameters.outsideColor)
-
-    for(let i = 0; i < parameters.count; i++)
-    {
-        const i3 = i * 3
-
-        // Position
-        const radius = Math.random() * parameters.radius
-
-        const branchAngle = (i % parameters.branches) / parameters.branches * Math.PI * 2
-
-        const randomX = Math.pow(Math.random(), parameters.randomnessPower) * (Math.random() < 0.5 ? 1 : - 1) * parameters.randomness * radius
-        const randomY = Math.pow(Math.random(), parameters.randomnessPower) * (Math.random() < 0.5 ? 1 : - 1) * parameters.randomness * radius
-        const randomZ = Math.pow(Math.random(), parameters.randomnessPower) * (Math.random() < 0.5 ? 1 : - 1) * parameters.randomness * radius
-
-        positions[i3    ] = Math.cos(branchAngle) * radius
-        positions[i3 + 1] = 0
-        positions[i3 + 2] = Math.sin(branchAngle) * radius
-    
-        randomness[i3    ] = randomX
-        randomness[i3 + 1] = randomY
-        randomness[i3 + 2] = randomZ
-
-        // Color
-        const mixedColor = insideColor.clone()
-        mixedColor.lerp(outsideColor, radius / parameters.radius)
-
-        colors[i3    ] = mixedColor.r
-        colors[i3 + 1] = mixedColor.g
-        colors[i3 + 2] = mixedColor.b
-
-        // Scale
-        scales[i] = Math.random()
-    }
-
-    geometry.setAttribute('position', new THREE.BufferAttribute(positions, 3))
-    geometry.setAttribute('aRandomness', new THREE.BufferAttribute(randomness, 3))
-    geometry.setAttribute('color', new THREE.BufferAttribute(colors, 3))
-    geometry.setAttribute('aScale', new THREE.BufferAttribute(scales, 1))
-
-    /**
-     * Material
-     */
-    material = new THREE.ShaderMaterial({
-        depthWrite: false,
-        blending: THREE.AdditiveBlending,
-        vertexColors: true,
-        uniforms:
+        if(child instanceof THREE.Mesh && child.material instanceof THREE.MeshStandardMaterial)
         {
-            uTime: { value: 0 },
-            uSize: { value: 20 * renderer.getPixelRatio() }
-        },    
-        vertexShader: galaxyVertexShader,
-        fragmentShader: galaxyFragmentShader
+            child.material.envMapIntensity = 5
+            child.material.needsUpdate = true
+            child.castShadow = true
+            child.receiveShadow = true
+        }
     })
-
-    /**
-     * Points
-     */
-    points = new THREE.Points(geometry, material)
-    scene.add(points)
 }
 
-gui.add(parameters, 'count').min(100).max(1000000).step(100).onFinishChange(generateGalaxy)
-gui.add(parameters, 'radius').min(0.01).max(20).step(0.01).onFinishChange(generateGalaxy)
-gui.add(parameters, 'branches').min(2).max(20).step(1).onFinishChange(generateGalaxy)
-gui.add(parameters, 'randomness').min(0).max(2).step(0.001).onFinishChange(generateGalaxy)
-gui.add(parameters, 'randomnessPower').min(1).max(10).step(0.001).onFinishChange(generateGalaxy)
-gui.addColor(parameters, 'insideColor').onFinishChange(generateGalaxy)
-gui.addColor(parameters, 'outsideColor').onFinishChange(generateGalaxy)
+/**
+ * Environment map
+ */
+const environmentMap = cubeTextureLoader.load([
+    '/textures/environmentMaps/0/px.jpg',
+    '/textures/environmentMaps/0/nx.jpg',
+    '/textures/environmentMaps/0/py.jpg',
+    '/textures/environmentMaps/0/ny.jpg',
+    '/textures/environmentMaps/0/pz.jpg',
+    '/textures/environmentMaps/0/nz.jpg'
+])
+environmentMap.encoding = THREE.sRGBEncoding
+
+scene.background = environmentMap
+scene.environment = environmentMap
+
+/**
+ * Material
+ */
+
+// Textures
+const mapTexture = textureLoader.load('/models/LeePerrySmith/color.jpg')
+mapTexture.encoding = THREE.sRGBEncoding
+
+const normalTexture = textureLoader.load('/models/LeePerrySmith/normal.jpg')
+
+// Material
+const material = new THREE.MeshStandardMaterial( {
+    map: mapTexture,
+    normalMap: normalTexture
+})
+
+/**
+ * Models
+ */
+gltfLoader.load(
+    '/models/LeePerrySmith/LeePerrySmith.glb',
+    (gltf) =>
+    {
+        // Model
+        const mesh = gltf.scene.children[0]
+        mesh.rotation.y = Math.PI * 0.5
+        mesh.material = material
+        scene.add(mesh)
+
+        // Update materials
+        updateAllMaterials()
+    }
+)
+
+/**
+ * Lights
+ */
+const directionalLight = new THREE.DirectionalLight('#ffffff', 3)
+directionalLight.castShadow = true
+directionalLight.shadow.mapSize.set(1024, 1024)
+directionalLight.shadow.camera.far = 15
+directionalLight.shadow.normalBias = 0.05
+directionalLight.position.set(0.25, 2, - 2.25)
+scene.add(directionalLight)
 
 /**
  * Sizes
@@ -154,9 +129,7 @@ window.addEventListener('resize', () =>
  */
 // Base camera
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
-camera.position.x = 3
-camera.position.y = 3
-camera.position.z = 3
+camera.position.set(4, 1, - 4)
 scene.add(camera)
 
 // Controls
@@ -167,15 +140,17 @@ controls.enableDamping = true
  * Renderer
  */
 const renderer = new THREE.WebGLRenderer({
-    canvas: canvas
+    canvas: canvas,
+    antialias: true
 })
+renderer.shadowMap.enabled = true
+renderer.shadowMap.type = THREE.PCFShadowMap
+renderer.physicallyCorrectLights = true
+renderer.outputEncoding = THREE.sRGBEncoding
+renderer.toneMapping = THREE.ACESFilmicToneMapping
+renderer.toneMappingExposure = 1
 renderer.setSize(sizes.width, sizes.height)
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
-
-/**
- * Generate the first galaxy
- */
-generateGalaxy()
 
 /**
  * Animate
@@ -185,9 +160,6 @@ const clock = new THREE.Clock()
 const tick = () =>
 {
     const elapsedTime = clock.getElapsedTime()
-
-    // Update material
-    material.uniforms.uTime.value = elapsedTime + 400
 
     // Update controls
     controls.update()
