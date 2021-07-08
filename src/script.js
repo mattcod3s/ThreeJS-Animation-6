40bc03d59cfc311883fe0966db46ce469843feb7
MattCod3s <matthew.kostka08@gmail.com>
2021-07-08 22:57:59 +0100

    Lesson 28 on Modified Materials started

--- src/script.js
+++ src/script.js
@@ -4,6 +4,8 @@ import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js'
 import * as dat from 'dat.gui'
 
+// Lesson start
+
 /**
  * Base
  */
